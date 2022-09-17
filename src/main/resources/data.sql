14338ba08e7a985670328f825126fcfea8380a1f
cts1pod <113626033+cts1pod@users.noreply.github.com>
2022-09-17 14:27:42 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into user (userid,upassword,uname) values ('admin','admin','admin');
-
+insert into user (userid,upassword,uname) values ('anagha','anagha','anagha');
 insert into user (userid,upassword,uname) values ('admin1','admin1','admin1');
 insert into user (userid,upassword,uname) values ('admin2','admin2','admin2');
 insert into user (userid,upassword,uname) values ('admin3','admin3','admin3');
